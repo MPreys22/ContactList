ec011217c133288081294bd7e87051fc81a7610f
mikemulla22 <michael.preys@menloschool.org>
2023-01-11 21:04:53 -0800

    Person Class done

--- src/Person.java
+++ src/Person.java
@@ -9,4 +9,30 @@
 
 public class Person {
     // TODO: Complete the Person class
+    // Broader instance variables applying to all types of people
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Person(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String toString() {
+        return getFirstName() + " " + getLastName() + " - #" + getPhoneNumber();
+    }
 }
